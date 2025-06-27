495f460b666744975c99468107602a95dec1ef52
Juan Flores <elgalleto12393@gmail.com>
2025-06-27 01:49:57 -0600

    fix: add android package and eas projectId to app configuration

--- app.config.ts
+++ app.config.ts
@@ -5,8 +5,14 @@ export default {
     name: "FMRU App",
     slug: "tu-app",
     version: "1.0.0",
+    android: {
+      package: "com.darkvenom.fmruapp"
+    },
     extra: {
       EXPO_PUBLIC_API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
+      eas: {
+        projectId: "d256492f-2efe-4e7f-9d8c-493a8df249d2"
+      }
     },
   },
 };
