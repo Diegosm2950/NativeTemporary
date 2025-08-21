fbd377ba7346a831cee7a00b169c8973ff10b271
Juan Flores <elgalleto12393@gmail.com>
2025-08-21 13:22:19 -0600

    chore: update app version to 1.1.0

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ const config = {
   name: "Rugby México",
   slug: "Ruby-Mexico",
   owner: "lolasux",
-  version: "1.0.0",
+  version: "1.1.0",
   orientation: "portrait",
   userInterfaceStyle: "automatic",
   scheme: "rugby-mexico",
